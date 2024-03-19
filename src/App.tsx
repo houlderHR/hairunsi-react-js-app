0138454506a85430174b76c91211da483d6a34c2
Hairun <safidytsiory@hairun-technology.com>
2024-03-19 17:30:38 +0300

    feat: init react socle

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+     <p className="p-4 bg-primary text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus numquam excepturi labore repellat distinctio possimus magnam perspiciatis fuga rem odit, nam minima. Accusantium maiores facere voluptas totam similique recusandae magni!</p>
+    </>
+  )
+}
+
+export default App
