8e58504098dc74ad59076d2dfc22e0a99add576e
diary <adiary@hairun-technology.com>
2024-03-25 11:03:43 +0300

    fix: config tailwind

--- .history/front/tailwind.config_20240325110030.js
+++ .history/front/tailwind.config_20240325110030.js
@@ -7,15 +7,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#12103B',     
+        "primary":"#12103B",
       },
-      fontFamily: {
-        ubuntu: ['Ubuntu', 'sans-serif'],
-      },
-      spacing: {
-        ssm: '20px',
-        llg: '62px',
-      }
     },
   },
   plugins: [],
