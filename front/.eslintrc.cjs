bcfcad86dca7da837d31b7a152d236a0d2625847
Tolotra Andrianalimanana <tolotra@hairun-technology.com>
2024-03-27 17:28:26 +0300

    🚨 Add/edit linter rules

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -4,13 +4,44 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  plugins: ['react', '@typescript-eslint', 'import', 'simple-import-sort'],
+  parserOptions: {
+    project: './tsconfig.json',
   },
+  rules: {
+    'import/no-unresolved': 'error',
+    'import/order': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [['^\\u0000', '^@?\\w', '^[^.]', '^\\.']],
+      },
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
+  ignorePatterns: [
+    '.eslintrc.cjs',
+    '**/node_modules/*',
+    '**/generated/*',
+    '**/*.d.ts',
+    '**/dist/*',
+    'vite.config.ts',
+  ],
 };
