997d95aa001038024f9ae1a108ec2716a1f93d83
Hairun <safidytsiory@hairun-technology.com>
2024-03-27 17:11:37 +0300

    add dropdown to modal role

--- front/src/shared/authenticated/Modal/CreateModal/CreateModalType/CreateModalType.tsx
+++ front/src/shared/authenticated/Modal/CreateModal/CreateModalType/CreateModalType.tsx
@@ -1,25 +1,29 @@
+import { useState } from 'react';
 import CreateModal from '..';
 import { ModalShowState } from '../../../../../utils/type/ModalShowType';
 import Input from '../../../Input';
 import InputIcon from '../../../Input/InputIcon';
+import DropDown from './DropDown';
 
 interface CreateModalTypeProps {
   setShowModal: React.Dispatch<React.SetStateAction<ModalShowState>>;
 }
 
 const CreateModalType: React.FC<CreateModalTypeProps> = ({ setShowModal }) => {
+  const [show, setShow] = useState(false);
   return (
     <>
       <CreateModal setShowModal={setShowModal}>
         <div className="flex gap-4 flex-col w-full">
           <Input type="text" placeholder="Nom du type" />
-          <div>
+          <div onClick={() => setShow((s) => !s)} className="relative">
             <InputIcon
               placeholder="Rôle"
               additionalClass="py-1 hover:bg-gray-50"
               additionalInputClass="text-base"
               icon="search"
             />
+            {show && <DropDown />}
           </div>
         </div>
       </CreateModal>
