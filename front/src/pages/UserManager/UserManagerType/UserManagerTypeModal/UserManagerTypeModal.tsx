1c22f0b18763a85d3e567de57593e1361cb0726a
Hairun <safidytsiory@hairun-technology.com>
2024-04-04 16:38:54 +0300

    ✨ Add delete modal for user manager type

--- front/src/pages/UserManager/UserManagerType/UserManagerTypeModal/UserManagerTypeModal.tsx
+++ front/src/pages/UserManager/UserManagerType/UserManagerTypeModal/UserManagerTypeModal.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { ModalShowStateType } from '../../../../shared/authenticated/Modal';
+import DeleteModal from '../../../../shared/authenticated/Modal/DeleteModal';
 import { UserType } from '../constants';
 import CreateTypeModal from './CreateTypeModal';
 import UpdateTypeModal from './UpdateTypeModal';
@@ -25,6 +26,16 @@ const UserManagerTypeModal: FC<UserManagerTypeModalProps> = ({
 
   if (user && modalState === ModalShowStateType.UPDATE) {
     return <UpdateTypeModal onClose={onClose} user={user} />;
+  }
+  if (modalState === ModalShowStateType.DELETE) {
+    return (
+      <DeleteModal
+        icon="role"
+        description=" Vous êtes sur le point de supprimer ce type d’utilisateur."
+        confirmation=" Etes-vous sûr de vouloir supprimer ce type d’utilisateur?"
+        onClose={onClose}
+      />
+    );
   }
 
   return null;
--- front/src/shared/authenticated/CardUserManager/CardType/CardType.tsx
+++ front/src/shared/authenticated/CardUserManager/CardType/CardType.tsx
@@ -7,11 +7,22 @@ interface CardTypeProps {
   user: UserType;
   iconVisible?: boolean;
   openUpdateModal: () => void;
+  openDeleteModal: () => void;
 }
 
 // CardType
-const CardType: FC<CardTypeProps> = ({ user, openUpdateModal, iconVisible = false }) => (
-  <CardUserManager openUpdateModal={openUpdateModal} title={user.title} iconVisible={iconVisible}>
+const CardType: FC<CardTypeProps> = ({
+  user,
+  openUpdateModal,
+  openDeleteModal,
+  iconVisible = false,
+}) => (
+  <CardUserManager
+    openDeleteModal={openDeleteModal}
+    openUpdateModal={openUpdateModal}
+    title={user.title}
+    iconVisible={iconVisible}
+  >
     <div className="flex mt-4 flex-row justify-start gap-x-4">
       <Icon name="user-guard" height={22} width={18} />
       <p className="text-base">{user.name}</p>
