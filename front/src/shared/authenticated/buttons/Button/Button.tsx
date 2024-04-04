81d6dd6704cae39fbdd68ab5b5f833a4b8db649b
diary <adiary@hairun-technology.com>
2024-04-04 12:55:21 +0300

    fix:switch on classname type button

--- front/src/shared/authenticated/buttons/Button/Button.tsx
+++ front/src/shared/authenticated/buttons/Button/Button.tsx
@@ -19,99 +19,40 @@ interface ButtonProps {
 }
 
 const Button: FC<ButtonProps> = ({ icon, title, className, onClick, variant, iconSize = 14 }) => {
+  let classNameValue: string = '';
   switch (variant) {
     case ButtonVariant.PRIMARY:
-      return (
-        <button
-          type="button"
-          className={twMerge(
-            className,
-            'w-full rounded flex justify-center py-3 bg-primary text-white font-medium justify-items-center gap-6',
-          )}
-          onClick={onClick}
-        >
-          {icon && (
-            <span className="mt-1">
-              <Icon name={icon} size={iconSize} />
-            </span>
-          )}
-          <span>{title}</span>
-        </button>
-      );
+      classNameValue =
+        'w-full rounded flex justify-center py-3 bg-primary text-white font-medium justify-items-center gap-6';
+      break;
     case ButtonVariant.SECONDARY:
-      return (
-        <button
-          type="button"
-          className={twMerge(
-            className,
-            'md:min-w-60 hover:bg-secondary duration-300 leading-3 flex flex-row gap-x-3 justify-center items-center bg-black-1 py-3.5 pl-8 pr-7 rounded text-xs text-white',
-          )}
-          onClick={onClick}
-        >
-          {icon && (
-            <span>
-              <Icon name={icon} size={iconSize} />
-            </span>
-          )}
-          <span>{title}</span>
-        </button>
-      );
+      classNameValue =
+        'md:min-w-60 hover:bg-secondary duration-300 leading-3 flex flex-row gap-x-3 justify-center items-center bg-black-1 py-3.5 pl-8 pr-7 rounded text-xs text-white';
+      break;
     case ButtonVariant.SECONDARY_1:
-      return (
-        <button
-          type="button"
-          className={twMerge(
-            className,
-            'w-full bg-secondary py-4 text-sm rounded flex justify-center mt-4 font-medium duration-300 text-base text-white',
-          )}
-          onClick={onClick}
-        >
-          {icon && (
-            <span>
-              <Icon name={icon} size={iconSize} />
-            </span>
-          )}
-          <span>{title}</span>
-        </button>
-      );
+      classNameValue =
+        'w-full bg-secondary py-4 text-sm rounded flex justify-center mt-4 font-medium duration-300 text-base text-white';
+      break;
     case ButtonVariant.DANGER:
-      return (
-        <button
-          type="button"
-          className={twMerge(
-            className,
-            'w-full rounded flex justify-center py-3 bg-danger-2 text-white text-white font-medium justify-items-center gap-6',
-          )}
-          onClick={onClick}
-        >
-          {icon && (
-            <span className="mt-1">
-              <Icon name={icon} size={iconSize} />
-            </span>
-          )}
-          <span>{title}</span>
-        </button>
-      );
-
+      classNameValue =
+        'w-full rounded flex justify-center py-3 bg-danger-2 text-white text-white font-medium justify-items-center gap-6';
+      break;
     default:
-      return (
-        <button
-          type="button"
-          className={twMerge(
-            className,
-            'w-full rounded flex justify-center py-3 bg-transparent border border-gray-1 text-black-1 justify-items-center gap-6',
-          )}
-          onClick={onClick}
-        >
-          {icon && (
-            <span className="mt-1">
-              <Icon name={icon} size={iconSize} />
-            </span>
-          )}
-          <span>{title}</span>
-        </button>
-      );
+      classNameValue =
+        'w-full rounded flex justify-center py-3 bg-transparent border border-gray-1 text-black-1 justify-items-center gap-6';
+      break;
   }
+
+  return (
+    <button type="button" className={twMerge(className, classNameValue)} onClick={onClick}>
+      {icon && (
+        <span className="mt-1">
+          <Icon name={icon} size={iconSize} />
+        </span>
+      )}
+      <span>{title}</span>
+    </button>
+  );
 };
 
 export default Button;
