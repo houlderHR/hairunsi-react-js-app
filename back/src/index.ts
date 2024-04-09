ff2e87603b6bab1fc893bf9bd99c3cc70f560bdd
jenny.hairun <jenny@hairun-technology.com>
2024-04-09 14:40:53 +0300

    🔥 Env file

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,15 +1,14 @@
-import { Express, Request, Response } from "express";
+import { Express } from 'express';
+import { config } from 'dotenv';
 import express = require('express');
-import dotenv = require('dotenv')
-import bodyParser = require("body-parser");
+import bodyParser = require('body-parser');
 
-
-dotenv.config();
+config();
 
 const port = process.env.PORT || 8080;
 const app: Express = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
