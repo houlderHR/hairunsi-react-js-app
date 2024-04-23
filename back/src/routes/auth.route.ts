a5c6233765b8bb641e58bc9b8fef94dabe1d6f4d
Hairun <safidytsiory@hairun-technology.com>
2024-04-23 10:20:43 +0300

    ✨ Add config .env in auth reset password

--- back/src/routes/auth.route.ts
+++ back/src/routes/auth.route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import AuthController from '../controllers/auth.controller';
+require('../utils/configEnv');
 
 require('../utils/configEnv');
 const authRoute = Router();
