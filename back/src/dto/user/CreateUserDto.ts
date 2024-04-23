e96e03e6d1255366a2a99760923554cd40532e89
jenny.hairun <jenny@hairun-technology.com>
2024-04-23 10:05:34 +0300

    🏷️ Table user

--- back/src/dto/user/CreateUserDto.ts
+++ back/src/dto/user/CreateUserDto.ts
@@ -2,15 +2,22 @@ import { IsDateString, IsString, IsUUID, MaxLength, MinLength } from 'class-vali
 import { UUID } from 'crypto';
 
 class CreateUserDto {
-  @IsString({ message: 'Le nom doit etre renseigner' })
+  @IsString({ message: 'Le nom doit etre renseigné' })
   @MinLength(1, { message: 'Le nom doit comporter au moin une caractère' })
   @MaxLength(255, { message: 'Le nom ne doit pas dépasser les 255 caractères' })
   firstname: string;
 
-  @IsString({ message: 'Le prenom doit etre renseigner' })
+  @IsString({ message: 'Le prenom doit etre renseigné' })
   @MinLength(1, { message: 'Le prenom doit comporter au moin une caractère' })
   @MaxLength(255, { message: 'Le prenom ne doit pas dépasser les 255 caractères' })
   lastname: string;
+
+  @IsString({ message: "L'adresse email doit etre renseignée" })
+  @MinLength(1, { message: "L'adresse email doit comporter au moin une caractère" })
+  @MaxLength(255, { message: "L'adresse email ne doit pas dépasser les 255 caractères" })
+  email: string;
+
+  password: string;
 
   @IsDateString({}, { message: 'Le format du date est incorrect' })
   birth_date: Date;
