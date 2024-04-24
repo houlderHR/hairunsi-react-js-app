523eb23a0121806b6d4fcf46eba8e704d65f7c2c
diary <adiary@hairun-technology.com>
2024-04-24 16:59:24 +0300

    check error on sending email

--- front/src/pages/ForgotPassword/ForgotPassword.tsx
+++ front/src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,24 +1,37 @@
-import { FC } from 'react';
+import axios from 'axios';
+import { FC, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import useSendMail from '../../hooks/useAuth';
 import Input from '../../shared/inputs/Input';
 import UserAuthenticationLayout from '../../shared/UserAuthenticationLayout';
 import Button from '../../shared/UserAuthenticationLayout/Button';
+import REGEX_MAIL_HAIRUN from '../../utils/regex';
 
 export type InputField = {
   email: string;
 };
 
 const ForgotPassword: FC = () => {
-  const { control, handleSubmit } = useForm<InputField>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputField>();
   const mutation = useSendMail();
+  const navigate = useNavigate();
+  const [errorAxios, setErrorAxios] = useState('');
 
   const onSubmit = handleSubmit(async (data) => {
     try {
-      const result = await mutation.mutateAsync(data);
-      console.log(result);
+      await mutation.mutateAsync(data);
+      navigate('/check-email');
     } catch (error) {
-      console.log(error);
+      if (axios.isAxiosError(error)) {
+        setErrorAxios(error.response?.data.error);
+      } else {
+        setErrorAxios('An error occurred');
+      }
     }
   });
 
@@ -30,11 +43,22 @@ const ForgotPassword: FC = () => {
       subTitle="Entrer dans le champ correspondant l'adresse e-mail que vous utilisez habituellement pour changer votre mot de passe."
       contentTitle="Récupération"
     >
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} className="w-full">
+        <p className="text-sm text-gray-1 mb-12 font-medium leading-4">
+          Saisissez votre adresse e-mail dans le champ ci-dessous.
+        </p>
+
         <Controller
           name="email"
           control={control}
           defaultValue=""
+          rules={{
+            required: { value: true, message: 'Remplir le champ' },
+            pattern: {
+              value: REGEX_MAIL_HAIRUN,
+              message: 'Le mail doit se terminer par @hairun-technology.com',
+            },
+          }}
           render={({ field: { onChange, onBlur, value } }) => (
             <Input
               type="email"
@@ -46,6 +70,8 @@ const ForgotPassword: FC = () => {
             />
           )}
         />
+        {errors.email && <p className="text-red-500 text-sm pt-2">{errors.email.message}</p>}
+        {errorAxios && <p className="text-red-500 text-sm pt-2">{errorAxios}</p>}
         <Button type="submit">Envoyer</Button>
       </form>
     </UserAuthenticationLayout>
