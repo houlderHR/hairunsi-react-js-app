5c6c51d1931e12172212d58ed5ae805fde26edc7
Hairun <safidytsiory@hairun-technology.com>
2024-05-16 18:01:20 +0300

    ✨ Add restriction in user manager type

--- front/src/hooks/useUserPermission.ts
+++ front/src/hooks/useUserPermission.ts
@@ -4,7 +4,10 @@ import UserContext from '../shared/authenticated/userContext';
 const useUserPermission = () => {
   const user = useContext(UserContext);
 
-  return user?.post.department.role.permissions;
+  const allowPermission = (permission: string): boolean =>
+    user?.post.department.role.permissions?.map(({ name }) => name).includes(permission) ?? false;
+
+  return { permissions: user?.post.department.role.permissions, allowPermission };
 };
 
 export default useUserPermission;
---
+++ front/src/utils/permissions.ts
@@ -0,0 +1,10 @@
+const PERMISSIONS = {
+  removeAll: 'Suppression tout utilisateur',
+  viewAll: 'Accès tout utilisateur',
+  createAll: 'Création tout utilisateur',
+  updateAll: 'Modification tout utilisateur',
+  viewUsers: 'Accès utilisateur',
+  updateUser: 'Modification utilisateur',
+};
+
+export default PERMISSIONS;
