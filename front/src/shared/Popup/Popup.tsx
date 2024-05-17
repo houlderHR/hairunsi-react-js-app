39a708f602c204944e47ae92fda764606addcea7
diary <adiary@hairun-technology.com>
2024-05-17 17:48:35 +0300

    get new feature

--- front/src/shared/Popup/Popup.tsx
+++ front/src/shared/Popup/Popup.tsx
@@ -5,7 +5,7 @@ interface PopupProps {
 }
 
 const Popup: FC<PopupProps> = ({ name }) => (
-  <div className="px-3 border md:hidden text-black-1 absolute left-full ml-4 hidden group-hover:block sm:group-hover:hidden border-gray-200 rounded bg-gray-50 py-2">
+  <div className="px-3 border md:hidden text-black-1 absolute left-full ml-4 hidden group-hover:block sm:group-hover:hidden border-gray-200 rounded bg-gray-50 py-2 z-[505]">
     {name}
   </div>
 );
