19e32511f1b8a58fd4da07256177ece2999bc818
diary <adiary@hairun-technology.com>
2024-05-17 14:23:55 +0300

    🔧 configuration jest

---
+++ back/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: './',
+  coverageDirectory: '<rootDir>/coverage',
+  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/build'],
+  testMatch: ['<rootDir>/src/__tests__/*.test.ts'],
+  verbose: true,
+  forceExit: false,
+};
