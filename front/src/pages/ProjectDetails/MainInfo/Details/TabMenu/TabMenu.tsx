cfa564cab4ab73f1447e7be45e04316575fd61fd
diary <adiary@hairun-technology.com>
2024-06-03 09:51:17 +0300

    ✨ side search integration

--- front/src/pages/ProjectDetails/MainInfo/Details/TabMenu/TabMenu.tsx
+++ front/src/pages/ProjectDetails/MainInfo/Details/TabMenu/TabMenu.tsx
@@ -1,10 +1,11 @@
 import React, { FC, useState } from 'react';
 
-type tabsType = {
-    title: string, content: React.ReactNode
-}
+type TabsType = {
+  title: string;
+  content: React.ReactNode;
+};
 
-const TabMenu: FC<{tabs: tabsType[]}> = ({ tabs }) => {
+const TabMenu: FC<{ tabs: TabsType[] }> = ({ tabs }) => {
   const [activeTab, setActiveTab] = useState(0);
 
   const handleTabClick = (index: number) => {
@@ -13,26 +14,24 @@ const TabMenu: FC<{tabs: tabsType[]}> = ({ tabs }) => {
 
   return (
     <div className=" p-4 h-full relative w-full">
-        <div className='absolute z-40 rounded-t-xl top-0 right-0 !bg-white w-full !h-20'>
-        </div>
-        <div className="fixed z-50 p-1 z-50 space-x-4 bg-gray-8 rounded-xl">
-                {tabs.map((tab, index) => (
-                <button
-                    key={index}
-                    onClick={() => handleTabClick(index)}
-                    className={`px-4 py-3 rounded-xl focus:outline-none ${
-                    activeTab === index ? 'bg-white text-secondary-2' : 'text-gray-1'
-                    }`}
-                >
-                    {tab.title}
-                </button>
-                ))}
-        </div>
-        <div  className=" absolute z-32 top-2 right-0 h-full w-full rounded-xl overflow-auto">
-            <div className="h-full pt-[72px] ">
-                {tabs[activeTab].content}
-            </div>
-        </div>
+      <div className="absolute z-40 rounded-t-xl top-0 right-0 !bg-white w-full !h-20" />
+      <div className="fixed z-50 p-1 z-50 space-x-4 bg-gray-8 rounded-xl">
+        {tabs.map((tab, index) => (
+          <button
+            type="button"
+            key={tab.title}
+            onClick={() => handleTabClick(index)}
+            className={`px-4 py-3 rounded-xl focus:outline-none ${
+              activeTab === index ? 'bg-white text-secondary-2' : 'text-gray-1'
+            }`}
+          >
+            {tab.title}
+          </button>
+        ))}
+      </div>
+      <div className=" absolute z-32 top-2 right-0 h-full w-full rounded-xl overflow-auto">
+        <div className="h-full pt-[72px] ">{tabs[activeTab].content}</div>
+      </div>
     </div>
   );
 };
--- front/src/pages/ProjectDetails/MainInfo/SideSearch/SideSearch.tsx
+++ front/src/pages/ProjectDetails/MainInfo/SideSearch/SideSearch.tsx
@@ -1,17 +1,38 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import InputIcon from '../../../../shared/inputs/InputIcon';
 
-const SideSearch: FC = () => (
-  <div className="h-full lg:w-[492px] bg-white border border-white-1 rounded-xl text-black-200">
-    <InputIcon
-      icon="search"
-      iconColor="text-gray-10"
-      additionalInputClass="placeholder:text-gray-1"
-      additionalClass="rounded-t-xl h-[56px] bg-white-2 "
-      placeholder="Rechercher pour ajouter des collaborateurs"
-    />
-    &nbsp;
-  </div>
-);
+const SideSearch: FC = () => {
+  const [users, setUsers] = useState([]);
+
+  return (
+    <div className="h-full w-full relative lg:w-[492px] bg-white border border-white-1 rounded-xl">
+      <InputIcon
+        icon="search"
+        iconColor="text-gray-10"
+        additionalInputClass="placeholder:text-gray-1"
+        additionalClass="rounded-t-xl h-[56px] bg-white-2 sticky top-0 right-0 z-50"
+        placeholder="Rechercher pour ajouter des collaborateurs"
+      />
+      <div className=" absolute z-32 top-0 right-0 h-full w-full rounded-xl overflow-auto">
+        <div
+          className={`h-full pt-14 overflow-auto ${
+            users.length === 0 && 'flex justify-center items-center'
+          }`}
+        >
+          {users.length === 0 ? (
+            <div className="text-center flex flex-col items-center">
+              <img src="/images/logo/empty-member.png" alt="" width={72} height={72} />
+              <span className="mt-4 text-xs text-secondary-3 leading-[13.79px] text-normal">
+                Aucun membre
+              </span>
+            </div>
+          ) : (
+            <div className="h-[2000px] bg-blue-200">fds</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SideSearch;
