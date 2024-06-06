3f0f40b7a62399dee5d7c5b6729ad205e3730609
diary <adiary@hairun-technology.com>
2024-06-06 08:19:42 +0200

    useParams on route project

--- front/src/pages/Project/ProjectDetails/MainInfo/Details/Contacts/AddContact/AddContact.tsx
+++ front/src/pages/Project/ProjectDetails/MainInfo/Details/Contacts/AddContact/AddContact.tsx
@@ -3,8 +3,8 @@ import Icon from '../../../../../../../shared/Icon';
 
 const AddContact: FC = () => (
   <div className="bg-white ">
-    <div className="text-gray-1 border-gray-9 rounded-md border-1 border-dashed py-3 px-4 border border-white-1 relative w-full flex justify-center gap-x-2 items-center">
-      <Icon name="add-solid" /> <span>Ajouter un contact</span>
+    <div className="group text-gray-1 border-[#bdbdbd] rounded-xl border border-dashed hover:border-solid hover:border-[#3E60C1] hover:text-[#3E60C1] py-3 px-4 hover:cursor-pointer relative w-full flex justify-center gap-x-2 items-center">
+      <Icon name="add-item"  className="text-[#808080] group-hover:bg-[#3E60C1] group-hover:text-white" /> <span>Ajouter un contact</span>
     </div>
   </div>
 );
