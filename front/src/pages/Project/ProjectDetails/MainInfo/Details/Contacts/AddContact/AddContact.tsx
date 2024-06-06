446bff45ba31b6f054700c71b4176a148f86f96b
diary <adiary@hairun-technology.com>
2024-06-06 09:36:40 +0300

    ✨ component contact

--- front/src/pages/Project/ProjectDetails/MainInfo/Details/Contacts/AddContact/AddContact.tsx
+++ front/src/pages/Project/ProjectDetails/MainInfo/Details/Contacts/AddContact/AddContact.tsx
@@ -1,11 +1,42 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Icon from '../../../../../../../shared/Icon';
+import Input from '../../../../../../../shared/inputs/Input';
 
-const AddContact: FC = () => (
-  <div className="bg-white ">
-    <div className="group text-gray-1 border-[#bdbdbd] rounded-xl border border-dashed hover:border-solid hover:border-[#3E60C1] hover:text-[#3E60C1] py-3 px-4 hover:cursor-pointer relative w-full flex justify-center gap-x-2 items-center">
-      <Icon name="add-item"  className="text-[#808080] group-hover:bg-[#3E60C1] group-hover:text-white" /> <span>Ajouter un contact</span>
+type AddContactProps = {
+  onClick?: () => void;
+};
+
+const AddContact: FC<AddContactProps> = ({ onClick }) => {
+  const [isCliked, setIsClicked] = useState(false);
+  const handleClick = () => {
+    setIsClicked(!isCliked);
+  };
+  return (
+    <div className="bg-white ">
+      {!isCliked ? (
+        <div
+          role="presentation"
+          onClick={handleClick}
+          className="group text-base text-gray-1 border-gray-9 rounded-lg border border-dashed hover:border-solid hover:border-[#3E60C1] hover:text-[#3E60C1] py-3 px-4 hover:cursor-pointer relative w-full flex justify-center gap-x-2 items-center"
+        >
+          <Icon
+            name="add-item"
+            className="text-[#808080] group-hover:bg-[#3E60C1] group-hover:text-white"
+          />
+          <span>Ajouter un contact</span>
+        </div>
+      ) : (
+        <div className="flex gap-x-4">
+          <Input type="text" />
+          <Input type="text" />
+          <Input type="text" />
+          <div className="flex items-center">
+            <Icon name="x" />
+            <Icon name="right" />
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
 export default AddContact;
--- front/src/pages/Project/ProjectDetails/MainInfo/Details/Contacts/Contacts.tsx
+++ front/src/pages/Project/ProjectDetails/MainInfo/Details/Contacts/Contacts.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import AddContact from './AddContact';
 
 const Contacts: FC = () => (
-  <div className="bg-white rounded-xl mb-2 border border-white-1 relative w-full h-full">
+  <div className="rounded-xl mb-2 border border-white-1 relative px-[64px] pt-[108px] w-full h-full bg-white">
     <AddContact />
   </div>
 );
