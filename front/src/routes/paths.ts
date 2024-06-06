27065172ccfd32358908fc159dd637d21617020a
Tsiory <safidytsiory@hairun-technology.com>
2024-06-06 09:20:16 +0300

    🐛 Fix route path

--- front/src/routes/paths.ts
+++ front/src/routes/paths.ts
@@ -14,9 +14,6 @@ const routes = {
               report: { path: 'report' },
             },
           },
-          report: {
-            path: '/report',
-          },
         },
       },
       leaveDay: { path: '/leave-day' },
